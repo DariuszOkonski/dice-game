f3ab1fb37b5c174566fd8a6f92b5a3782af5820a
Dariusz Okonski <darek200180@gmail.com>
2019-11-14 11:49:29 +0000

    Updates

---
+++ precache-manifest.c7a28e71a50aeee5fa8854fef5b66e0c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9ee062354af0bc35ee5a8f1a655e538",
+    "url": "/dice-game/index.html"
+  },
+  {
+    "revision": "abb966824b16faba3731",
+    "url": "/dice-game/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "fddf0237a3fa018092da",
+    "url": "/dice-game/static/js/2.ac204b98.chunk.js"
+  },
+  {
+    "revision": "abb966824b16faba3731",
+    "url": "/dice-game/static/js/main.47397ff7.chunk.js"
+  },
+  {
+    "revision": "9942223db4df487cf4ac",
+    "url": "/dice-game/static/js/runtime-main.19eb9274.js"
+  }
+]);
