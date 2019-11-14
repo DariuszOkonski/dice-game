6c1abf431bff7e2b12f11b3906d8f8e4dbf67412
Dariusz Okonski <darek200180@gmail.com>
2019-11-14 12:12:03 +0000

    display Die component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
+import Die from './Die';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello</h1>
-      </header>
+      <Die face='one' />
+      <Die face='two' />
     </div>
   );
 }
@@ -15,3 +15,5 @@ export default App;
 
 //https://fontawesome.com/start
 //https://fontawesome.com/icons/dice-one?style=solid
+//<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css"></link>
+//<i className="fas fa-dice-one"></i>
